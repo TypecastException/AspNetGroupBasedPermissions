a118d5552260f6dbbfa1d669d142af600182db65
xivsolutions <xivsolutions@gmail.com>
2014-02-10 20:46:51 -0600

    Added new required classes

---
+++ AspNetExtendingIdentityRoles/Models/ApplicationUserRole.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace AspNetExtendingIdentityRoles.Models
+{
+    public class ApplicationUserRole : IdentityUserRole
+    {
+        new public virtual ApplicationRole Role { get; set; }
+        new public virtual ApplicationUser User { get; set; }   
+    }
+}
