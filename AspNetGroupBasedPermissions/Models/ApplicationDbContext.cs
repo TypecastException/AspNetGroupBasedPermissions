4b063e0e9655d26f948fd2cf5d31808784f07f2a
xivsolutions <xivsolutions@gmail.com>
2014-02-16 11:06:27 -0600

    Add Group stuff to ApplicationDbContext

--- AspNetGroupBasedPermissions/Models/ApplicationDbContext.cs
+++ AspNetGroupBasedPermissions/Models/ApplicationDbContext.cs
@@ -1,18 +1,7 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity;
-using System.ComponentModel.DataAnnotations;
-using System.Collections.Generic;
-using System.Data.Entity;
 using System;
-using System.Data.Entity.Infrastructure;
+using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration;
-using System.Data.Entity.ModelConfiguration.Configuration;
-using System.Data.Entity.Validation;
-using System.Globalization;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 
 namespace AspNetGroupBasedPermissions.Models
 {
@@ -20,6 +9,7 @@ namespace AspNetGroupBasedPermissions.Models
     {
         // Add an instance IDbSet using the 'new' keyword:
         new public virtual IDbSet<ApplicationRole> Roles { get; set; }
+        public virtual IDbSet<Group> Groups { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection")
@@ -39,19 +29,32 @@ namespace AspNetGroupBasedPermissions.Models
             modelBuilder.Entity<IdentityUser>().ToTable("AspNetUsers");
 
             // Change TUser to ApplicationUser everywhere else - IdentityUser and ApplicationUser essentially 'share' the AspNetUsers Table in the database:
-            EntityTypeConfiguration<ApplicationUser> table = 
+            EntityTypeConfiguration<ApplicationUser> table =
                 modelBuilder.Entity<ApplicationUser>().ToTable("AspNetUsers");
 
             table.Property((ApplicationUser u) => u.UserName).IsRequired();
 
             // EF won't let us swap out IdentityUserRole for ApplicationUserRole here:
             modelBuilder.Entity<ApplicationUser>().HasMany<IdentityUserRole>((ApplicationUser u) => u.Roles);
-            modelBuilder.Entity<IdentityUserRole>().HasKey((IdentityUserRole r) => 
+            modelBuilder.Entity<IdentityUserRole>().HasKey((IdentityUserRole r) =>
                 new { UserId = r.UserId, RoleId = r.RoleId }).ToTable("AspNetUserRoles");
 
+
+            // Add the group stuff here:
+            modelBuilder.Entity<ApplicationUser>().HasMany<ApplicationUserGroup>((ApplicationUser u) => u.Groups);
+            modelBuilder.Entity<ApplicationUserGroup>().HasKey((ApplicationUserGroup r) => new { UserId = r.UserId, GroupId = r.GroupId }).ToTable("ApplicationUserGroups");
+
+            // And here:
+            modelBuilder.Entity<Group>().HasMany<ApplicationRoleGroup>((Group g) => g.Roles);
+            modelBuilder.Entity<ApplicationRoleGroup>().HasKey((ApplicationRoleGroup gr) => new { RoleId = gr.RoleId, GroupId = gr.GroupId }).ToTable("ApplicationRoleGroups");
+
+            // And Here:
+            EntityTypeConfiguration<Group> groupsConfig = modelBuilder.Entity<Group>().ToTable("Groups");
+            groupsConfig.Property((Group r) => r.Name).IsRequired();
+
             // Leave this alone:
-            EntityTypeConfiguration<IdentityUserLogin> entityTypeConfiguration = 
-                modelBuilder.Entity<IdentityUserLogin>().HasKey((IdentityUserLogin l) => 
+            EntityTypeConfiguration<IdentityUserLogin> entityTypeConfiguration =
+                modelBuilder.Entity<IdentityUserLogin>().HasKey((IdentityUserLogin l) =>
                     new { UserId = l.UserId, LoginProvider = l.LoginProvider, ProviderKey = l.ProviderKey }).ToTable("AspNetUserLogins");
 
             entityTypeConfiguration.HasRequired<IdentityUser>((IdentityUserLogin u) => u.User);
