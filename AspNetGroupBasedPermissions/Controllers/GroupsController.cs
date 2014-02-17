a516c76662e9678a929c488e7aa5f50f3ab20293
xivsolutions <xivsolutions@gmail.com>
2014-02-16 20:10:09 -0600

    Add Groups Controller

---
+++ AspNetGroupBasedPermissions/Controllers/GroupsController.cs
@@ -0,0 +1,162 @@
+﻿using AspNetGroupBasedPermissions.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace AspNetGroupBasedPermissions.Controllers
+{
+    public class GroupsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        [Authorize(Roles = "Admin, CanEditGroup, CanEditUser")]
+        public ActionResult Index()
+        {
+            return View(db.Groups.ToList());
+        }
+
+
+        [Authorize(Roles = "Admin, CanEditGroup, CanEditUser")]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Group group = db.Groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+
+        [Authorize(Roles = "Admin, CanEditGroup")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [Authorize(Roles = "Admin, CanEditGroup")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Name")] Group group)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Groups.Add(group);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(group);
+        }
+
+
+        [Authorize(Roles = "Admin, CanEditGroup")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Group group = db.Groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+
+        [Authorize(Roles = "Admin, CanEditGroup")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Name")] Group group)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(group).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(group);
+        }
+
+
+        [Authorize(Roles = "Admin, CanEditGroup")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Group group = db.Groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+
+        [Authorize(Roles = "Admin, CanEditGroup")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Group group = db.Groups.Find(id);
+            var idManager = new IdentityManager();
+            idManager.DeleteGroup(id);
+            return RedirectToAction("Index");
+        }
+
+
+        [Authorize(Roles = "Admin, CanEditGroup")]
+        public ActionResult GroupRoles(int id)
+        {
+            var group = db.Groups.Find(id);
+            var model = new SelectGroupRolesViewModel(group);
+            return View(model);
+        }
+
+
+        [HttpPost]
+        [Authorize(Roles = "Admin, CanEditGroup")]
+        [ValidateAntiForgeryToken]
+        public ActionResult GroupRoles(SelectGroupRolesViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var idManager = new IdentityManager();
+                var Db = new ApplicationDbContext();
+                var group = Db.Groups.Find(model.GroupId);
+                idManager.ClearGroupRoles(model.GroupId);
+
+                // Add each selected role to this group:
+                foreach (var role in model.Roles)
+                {
+                    if (role.Selected)
+                    {
+                        idManager.AddRoleToGroup(group.Id, role.RoleName);
+                    }
+                }
+                return RedirectToAction("index");
+            }
+            return View();
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
