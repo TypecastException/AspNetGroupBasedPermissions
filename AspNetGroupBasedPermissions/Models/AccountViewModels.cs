d73b5a38f54257b5bfb040f064a2f37435b6ac22
xivsolutions <xivsolutions@gmail.com>
2014-02-16 20:09:48 -0600

    Add new ViewModels

--- AspNetGroupBasedPermissions/Models/AccountViewModels.cs
+++ AspNetGroupBasedPermissions/Models/AccountViewModels.cs
@@ -121,49 +121,6 @@ namespace AspNetGroupBasedPermissions.Models
     }
 
 
-    public class SelectUserRolesViewModel
-    {
-        public SelectUserRolesViewModel()
-        {
-            this.Roles = new List<SelectRoleEditorViewModel>();
-        }
-
-
-        // Enable initialization with an instance of ApplicationUser:
-        public SelectUserRolesViewModel(ApplicationUser user)
-            : this()
-        {
-            this.UserName = user.UserName;
-            this.FirstName = user.FirstName;
-            this.LastName = user.LastName;
-
-            var Db = new ApplicationDbContext();
-
-            // Add all available roles to the list of EditorViewModels:
-            var allRoles = Db.Roles;
-            foreach (var role in allRoles)
-            {
-                // An EditorViewModel will be used by Editor Template:
-                var rvm = new SelectRoleEditorViewModel(role);
-                this.Roles.Add(rvm);
-            }
-
-            // Set the Selected property to true for those roles for 
-            // which the current user is a member:
-            foreach (var userRole in user.Roles)
-            {
-                var checkUserRole =
-                    this.Roles.Find(r => r.RoleName == userRole.Role.Name);
-                checkUserRole.Selected = true;
-            }
-        }
-
-        public string UserName { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public List<SelectRoleEditorViewModel> Roles { get; set; }
-    }
-
     // Used to display a single role with a checkbox, within a list structure:
     public class SelectRoleEditorViewModel
     {
@@ -216,4 +173,137 @@ namespace AspNetGroupBasedPermissions.Models
             this.Description = role.Description;
         }
     }
+
+
+    // Wrapper for SelectGroupEditorViewModel to select user group membership:
+    public class SelectUserGroupsViewModel
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<SelectGroupEditorViewModel> Groups { get; set; }
+
+        public SelectUserGroupsViewModel()
+        {
+            this.Groups = new List<SelectGroupEditorViewModel>();
+        }
+
+        public SelectUserGroupsViewModel(ApplicationUser user)
+            : this()
+        {
+            this.UserName = user.UserName;
+            this.FirstName = user.FirstName;
+            this.LastName = user.LastName;
+
+            var Db = new ApplicationDbContext();
+
+            // Add all available groups to the public list:
+            var allGroups = Db.Groups;
+            foreach (var role in allGroups)
+            {
+                // An EditorViewModel will be used by Editor Template:
+                var rvm = new SelectGroupEditorViewModel(role);
+                this.Groups.Add(rvm);
+            }
+
+            // Set the Selected property to true where user is already a member:
+            foreach (var group in user.Groups)
+            {
+                var checkUserRole =
+                    this.Groups.Find(r => r.GroupName == group.Group.Name);
+                checkUserRole.Selected = true;
+            }
+        }
+    }
+
+
+    // Used to display a single role group with a checkbox, within a list structure:
+    public class SelectGroupEditorViewModel
+    {
+        public SelectGroupEditorViewModel() { }
+        public SelectGroupEditorViewModel(Group group)
+        {
+            this.GroupName = group.Name;
+            this.GroupId = group.Id;
+        }
+
+        public bool Selected { get; set; }
+
+        [Required]
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+    }
+
+
+    public class SelectGroupRolesViewModel
+    {
+        public SelectGroupRolesViewModel()
+        {
+            this.Roles = new List<SelectRoleEditorViewModel>();
+        }
+
+
+        // Enable initialization with an instance of ApplicationUser:
+        public SelectGroupRolesViewModel(Group group)
+            : this()
+        {
+            this.GroupId = group.Id;
+            this.GroupName = group.Name;
+
+            var Db = new ApplicationDbContext();
+
+            // Add all available roles to the list of EditorViewModels:
+            var allRoles = Db.Roles;
+            foreach (var role in allRoles)
+            {
+                // An EditorViewModel will be used by Editor Template:
+                var rvm = new SelectRoleEditorViewModel(role);
+                this.Roles.Add(rvm);
+            }
+
+            // Set the Selected property to true for those roles for 
+            // which the current user is a member:
+            foreach (var groupRole in group.Roles)
+            {
+                var checkGroupRole =
+                    this.Roles.Find(r => r.RoleName == groupRole.Role.Name);
+                checkGroupRole.Selected = true;
+            }
+        }
+
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+        public List<SelectRoleEditorViewModel> Roles { get; set; }
+    }
+
+
+    public class UserPermissionsViewModel
+    {
+        public UserPermissionsViewModel()
+        {
+            this.Roles = new List<RoleViewModel>();
+        }
+
+
+        // Enable initialization with an instance of ApplicationUser:
+        public UserPermissionsViewModel(ApplicationUser user)
+            : this()
+        {
+            this.UserName = user.UserName;
+            this.FirstName = user.FirstName;
+            this.LastName = user.LastName;
+            foreach (var role in user.Roles)
+            {
+                var appRole = (ApplicationRole)role.Role;
+                var pvm = new RoleViewModel(appRole);
+                this.Roles.Add(pvm);
+            }
+        }
+
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<RoleViewModel> Roles { get; set; }
+    }
+
 }
